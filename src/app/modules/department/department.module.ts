ed2f986e5bd014aa473dec74f7e84452fa3102a2
MaksymKoloda <maksim741@ukr.net>
2021-11-27 21:51:57 +0200

    Implement creative connections tab

--- src/app/modules/department/department.module.ts
+++ src/app/modules/department/department.module.ts
@@ -1,15 +1,24 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { SharedModule } from "src/app/shared/shared.module";
+import { CreativeConnectionsComponent } from "./components/creative-connections/creative-connections.component";
+import { DownloadReportsComponent } from "./components/download-reports/download-reports.component";
+import { NewCreativeConnectionComponent } from "./components/new-creative-connection/new-creative-connection.component";
 import { NewPublicationComponent } from "./components/new-publication/new-publication.component";
 import { PublicationsComponent } from "./components/publications/publications.component";
 import { DepartmentRoutingModule } from "./department-routing.module";
 import { DepartmentComponent } from "./pages/department/department.component";
 import { DepartmentService } from "./services/department.service";
-import { DownloadReportsComponent } from './components/download-reports/download-reports.component';
 
 @NgModule({
-  declarations: [DepartmentComponent, NewPublicationComponent, PublicationsComponent, DownloadReportsComponent],
+  declarations: [
+    DepartmentComponent,
+    NewPublicationComponent,
+    PublicationsComponent,
+    DownloadReportsComponent,
+    CreativeConnectionsComponent,
+    NewCreativeConnectionComponent,
+  ],
   imports: [CommonModule, DepartmentRoutingModule, SharedModule],
   providers: [DepartmentService],
 })
