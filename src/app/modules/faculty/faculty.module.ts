c16140ccdaa52ff558b0071fa521c5fefb36e29c
MaksymKoloda <maksim741@ukr.net>
2021-12-04 19:55:44 +0200

    Implement reports on faculty page

--- src/app/modules/faculty/faculty.module.ts
+++ src/app/modules/faculty/faculty.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { FacultyRoutingModule } from './faculty-routing.module';
-
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { SharedModule } from "src/app/shared/shared.module";
+import { DownloadReportsComponent } from "./components/download-reports/download-reports.component";
+import { NewPublicationComponent } from "./components/new-publication/new-publication.component";
+import { PublicationsComponent } from "./components/publications/publications.component";
+import { FacultyRoutingModule } from "./faculty-routing.module";
+import { FacultyComponent } from "./pages/faculty/faculty.component";
+import { FacultyService } from "./services/faculty.service";
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    FacultyRoutingModule
-  ]
+  declarations: [
+    FacultyComponent,
+    DownloadReportsComponent,
+    PublicationsComponent,
+    NewPublicationComponent,
+  ],
+  imports: [CommonModule, FacultyRoutingModule, SharedModule],
+  providers: [FacultyService],
 })
-export class FacultyModule { }
+export class FacultyModule {}
